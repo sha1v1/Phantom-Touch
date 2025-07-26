db28fbf16f3a2a0b3177bfcff8c1938032627dcb
Shaivi Gandhi <shaivigandhi@yahoo.com>
2025-07-26 14:20:32 -0300

    added contour lines

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
+# if on a mac, turn off bluetooth (otherwise it connects to the iphone camera lol)
+
+cap = cv.VideoCapture(0,  cv.CAP_ANY)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+prev = None
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -15,8 +18,21 @@ while True:
         break
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    if prev is None:
+        prev = gray
+        continue
+    diff = cv.absdiff(prev, gray)
+    _, thresh = cv.threshold(diff, 25, 255, cv.THRESH_BINARY)
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if cv.contourArea(cnt) < 1000: continue
+        cv.drawContours(frame, [cnt], 0, (0,255,0), 3)
+
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    cv.imshow('phantom-touch', frame)
+    
+    prev = gray
+
     if cv.waitKey(1) == ord('q'):
         break
 
